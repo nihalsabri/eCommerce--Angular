a069e4f686a1f3a957f528a85c12c6063293547a
Nihal Sabri <nihalsabri60@gmail.com>
2025-09-24 15:29:34 +0300

    base structure - products cards

---
+++ src/app/models/iproducts.ts
@@ -0,0 +1,11 @@
+export interface Iproducts {
+ id:number;
+productName:string;
+productImgUrl:string;
+productQuantity:number;
+productPrice:number;
+categoryId:number;
+productDetails:string;
+
+
+}
